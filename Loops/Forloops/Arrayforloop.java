93a4b2691f9d27a146e079a3503f3b737afca477
rajeshreddy689-jpg <rajeshreddy689@gmail.com>
2025-09-12 18:42:51 -0500

    Adding Forloop files

---
+++ Loops/Forloops/Arrayforloop.java
@@ -0,0 +1,12 @@
+class Arrayforloop
+{
+    public static void main(String[] args)
+    {
+        String[] fruits = {"Apple", "Banana", "Cherry"};
+
+        for(int i = 0; i < fruits.length; i++)
+        {
+            System.out.println(fruits[i]);
+        }
+    }
+}
