d8409dbbf853a9090e44ba0446ef92eab69ec05b
rajeshreddy689-jpg <rajeshreddy689@gmail.com>
2025-09-12 13:08:08 -0500

    Initial commit - add project folders

---
+++ Concepts/Conditions.java
@@ -0,0 +1,46 @@
+class Conditions
+{
+    public static void main(String[] args)
+    {
+       /*  int x = 5;
+        int y = 12;
+
+        if(x < y)
+        {
+            System.out.println("Good");
+            System.out.println("XGood");
+        }
+        else
+        {
+            System.out.println("Bad");
+        } */
+    
+
+       /*  int x = 8;
+        int y = 9;
+        int z = 4;
+
+        if(x<z && z>x)
+        {
+            System.out.println("All Good");
+        }
+        else if(z<y && x<z)
+        {
+            System.out.println("Somewhat ok!");
+        }
+        else
+        {
+            System.out.println("Bad");
+        } */
+       int n = 4;
+       int result = 1;
+
+       if(n%2==0)
+            result = 10;
+       else
+            result = 20;
+        System.out.println(result);
+    }
+
+
+}
---
+++ Concepts/Hello.java
@@ -0,0 +1,24 @@
+class Hello
+{
+    public static void main(String a[])
+    {
+        int num = 48;                   //int is 4 byte in range means can only store 32 bits
+        int num1 = 12;
+        byte by = 127;                 //byte is 1 byte in range means can only store 8 bits
+        short sh = 558;               //int is 2 byte in range means can only store 16 bits
+        long l = 29876;              //long is 8 byte in range means can only store 64 bits
+        int result = num + num1;
+        double num2 = 5.6;
+        float num3 = 4.1f;
+        char c = 'R';
+        boolean b = true;
+        System.out.println("Hello Rajesh!");
+        System.out.println(num);
+        System.out.println(result);
+        System.out.println(num2);
+        System.out.println(num3);
+        System.out.println(c);
+        System.out.println(b);
+
+    }
+}
